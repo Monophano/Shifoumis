a3fa96c05fafd75423b8c54a0871230b97cc3bf1
Monophano <89103355+Monophano@users.noreply.github.com>
2023-03-28 07:14:54 +0200

    Add files via upload

--- src/Mode.c
+++ src/Mode.c
@@ -41,18 +41,18 @@ void player_vs_bot(void)
 
 		// choix du bot
 		srand(time(NULL));
-		int bot_choix = rand() % (3);
+		int bot_choix = 1 - rand() % (3 - 1);
 
 		// vérification de l'action réalisée
 		printf("\n");
 
 		if (player_choix == 1) {printf("Vous avez choisis la pierre\n");}
-		if (player_choix == 2) {printf("Vous avez choisis la feuille\n");}
-		if (player_choix == 3) {printf("Vous avez choisis les ciseaux\n");}
+		else if (player_choix == 2) {printf("Vous avez choisis la feuille\n");}
+		else if (player_choix == 3) {printf("Vous avez choisis les ciseaux\n");}
 
 		if (bot_choix == 1) {printf("Le bot a choisis la pierre\n");}
-		if (bot_choix == 2) {printf("Le bot a choisis la feuille\n");}
-		if (bot_choix == 3) {printf("Le bot a choisis les ciseaux\n");}
+		else if (bot_choix == 2) {printf("Le bot a choisis la feuille\n");}
+		else if (bot_choix == 3) {printf("Le bot a choisis les ciseaux\n");}
 		
 		printf("\n");
 
@@ -115,4 +115,100 @@ void player_vs_bot(void)
 // bot contre bot
 void bot_vs_bot(void)
 {
+	// nombre de round du shifoumis
+	int round;
+
+	printf("Choisissez le nombre de round que vous voulez jouer : ");
+	scanf("%d", &round);
+	printf("\n");
+
+	// Points des deux bots
+	int point_bot_un = 0;
+	int point_bot_deux = 0;	
+
+	// affichage des possibilités du joueur
+	printf("Bienvenue sur le mode de jeu player vs bot \n");
+	printf("Action du jeu : \n");
+	printf("	1 - Pierre \n");
+	printf("	2 - Feuille \n");
+	printf("	3 - Ciseaux \n");
+
+	// game loop
+	while (round != 0)
+	{	
+		//// choix des bot ////
+		srand(time(NULL));
+		// choix du bot un
+		int bot_choix_un = 1 - rand() % (3 - 1);
+
+		// choix du bot deux
+		int bot_choix_deux = 1 - rand() % (3 - 1);
+
+		// vérification de l'action réalisée
+		printf("\n");
+
+		if (bot_choix_un == 1) {printf("Vous avez choisis la pierre\n");}
+		else if (bot_choix_un == 2) {printf("Vous avez choisis la feuille\n");}
+		else if (bot_choix_un == 3) {printf("Vous avez choisis les ciseaux\n");}
+
+		if (bot_choix_deux == 1) {printf("Le bot a choisis la pierre\n");}
+		else if (bot_choix_deux == 2) {printf("Le bot a choisis la feuille\n");}
+		else if (bot_choix_deux == 3) {printf("Le bot a choisis les ciseaux\n");}
+		
+		printf("\n");
+
+		// vérification du joueur gagnant le point
+		/* 1 = pierre
+		 * 2 = feuille
+		 * 3 = ciseaux*/
+		// Joueur gagne un point
+		if (bot_choix_un == 1 && bot_choix_deux == 3)
+		{
+			printf("Bot 1 gagne 1 point");
+			point_bot_un += 1;	
+		}
+		else if (bot_choix_un == 2 && bot_choix_deux == 1)
+		{
+			printf("Bot 1 gagne 1 point");
+			point_bot_un += 1;
+		}
+		else if (bot_choix_un == 3 && bot_choix_deux == 2)
+		{
+			printf("Bot 1 gagne 1 point");
+			point_bot_un += 1;
+		}
+
+		// bot gagne un point
+		if (bot_choix_deux == 1 && bot_choix_un == 3)
+		{
+			printf("Bot 2 gagne 1 point\n");
+			point_bot_deux += 1;
+		}
+		else if (bot_choix_deux == 2 && bot_choix_un == 1)
+		{
+			printf("Bot 2 gagne 1 point\n");
+			point_bot_deux += 1;
+		}
+		else if (bot_choix_deux == 3 && bot_choix_un == 2)
+		{
+			printf("Bot 2 gagne 1 point\n");
+			point_bot_deux += 1;
+		}
+
+		// égalité
+		if (bot_choix_un == bot_choix_deux) {printf("Egalite\n");}
+		
+		printf("\n");
+
+		// nombre de round restant
+		round -= 1;
+		printf("Il reste %d round\n", round);
+
+		printf("\n");
+	}
+	
+	// vérification de qui a gagné
+	if (point_bot_un > point_bot_deux) {printf("Bot 1 a gagne");}
+	else if (point_bot_un == point_bot_deux) {printf("Egalite");}
+	else {printf("Bot 2 a gagne");}
 }
